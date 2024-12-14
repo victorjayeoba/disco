5d89a3331452ab7557554864c2635165617b2d00
Jayeoba Victor <victorjayeoba02@gmail.com>
2024-12-15 00:48:23 +0100

    adsense

--- app/components/Header.js
+++ app/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="fixed top-0 left-0 w-full bg-christmas-green text-snow-white shadow-lg z-50">
       <div className="container mx-auto flex items-center justify-between py-4 px-4">
-      <h2  className="text-2xl" >DISCO</h2>  <span className="animate-pulse">🌟</span> 
+      <h2  className="text-2xl" >DISCO  <span className="animate-pulse">🌟</span> </h2> 
        
         
       </div>
